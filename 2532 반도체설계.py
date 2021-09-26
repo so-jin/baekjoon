be04c19d877c6e18fe734b17102e4987548b1ca7
so-jin <52068107+so-jin@users.noreply.github.com>
2021-09-26 13:48:01 +0900

    Update 2532 반도체설계.py

--- 2532 반도체설계.py
+++ 2532 반도체설계.py
@@ -1,25 +1,23 @@
 from bisect import bisect_left, bisect_right
-# f = open("in.txt","r")
 
-# size = int(f.readline())
 size = int(input())
 # arr = list(map(int, (f.readline()).split()))
 arr = list(map(int,input().split()))
-lcs = []
+lis = []
 
+# lcs를 기억하는 것이 아니라, lcs의 길이만 알면 된다.
 for i in range(size):
-
     # 비어있으면 넣기
-    if len(lcs) == 0 :
-        lcs.append(arr[i])
+    if len(lis) == 0 :
+        lis.append(arr[i])
     # 마지막 요소보다 크면 추가
-    elif arr[i] > lcs[-1] :
-        lcs.append(arr[i])
+    elif arr[i] > lis[-1] :
+        lis.append(arr[i])
     #  right의 위치를 찾아서, right을 arr[i]로 바꾸기
     else:
 #         맞는 위치 찾아가기
 #         left = bisect_left(lcs,arr[i],0,len(lcs))
-        right = bisect_right(lcs, arr[i], 0, len(lcs))
-        lcs[right] = arr[i]
+        right = bisect_right(lis, arr[i], 0, len(lis))
+        lis[right] = arr[i]
 
-print(len(lcs))
+print(len(lis))
