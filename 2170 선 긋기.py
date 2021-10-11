208d7c2d974e6c172484ab274bce3d4a9d835fc8
so-jin <52068107+so-jin@users.noreply.github.com>
2021-10-11 23:09:47 +0900

    Create 2170 선 긋기.py

---
+++ 2170 선 긋기.py
@@ -0,0 +1,34 @@
+# f = open("in.txt","r")
+
+# num = int(f.readline())
+num = int(input())
+arr = []
+for i in range(num):
+    # arr.append(list(map(int, f.readline().split())))
+    a,b = map(int, input().split())
+    arr.append((a,b))
+
+arr.sort()
+
+# 초기 좌측, 우측 값 
+l = float("inf")
+r = float("-inf")
+
+
+sum = 0
+for str in arr:
+    # 우측 값보다 현재 좌측 값이 더 크면, string이 분리되는 것 
+    if str[0] > r:
+        # 새로운 string에 대해 탐색하므로 이전 string 길이 저장 
+        if r-l > 0:
+            sum+= r-l
+
+        l = str[0]
+        r = str[1]
+    # 시작점은 원래 string에 포함되나, 끝나는 지점이 다를 때, 길이 연장 
+    elif str[1]>r:
+        r = str[1]
+
+sum += r-l
+
+print(sum)
